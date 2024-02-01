66a84f19d22002eb9e031cb7c86a3237cbb1900f
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-02-01 22:15:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -21,9 +21,7 @@ const AppData = "tmp"
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         p := strings.TrimPrefix(r.URL.Path, "/")
-        if !regexp.MustCompile(`^[a-zA-Z0-9]+$`).MatchString(p) || len(p) > 16 {
-            handleIllegalPath(w, r)
-        } else {
+        if regexp.MustCompile(`^[a-zA-Z0-9]+$`).MatchString(p) && len(p) < 16 {
             ua := r.Header.Get("user-agent")
             f := filepath.Join(AppData, p)
             if r.Method == http.MethodPost {
@@ -31,27 +29,27 @@ func main() {
             } else {
                 handleGet(w, r, f, p, ua)
             }
+        } else {
+            if r.Method == http.MethodGet {
+                illegalPath(w, r)
+            }
         }
     })
     http.ListenAndServe(":10003", nil)
 }
 
-func handleIllegalPath(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodPost {
-    } else {
-        a := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        var p string
-        for i := 0; i < 4; i++ {
-            p += string(a[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(a))])
-        }
-        http.Redirect(w, r, "/"+p, http.StatusFound)
+func illegalPath(w http.ResponseWriter, r *http.Request) {
+    a := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    var p string
+    for i := 0; i < 4; i++ {
+        p += string(a[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(a))])
     }
+    http.Redirect(w, r, "/"+p, http.StatusFound)
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request, f string, p string, ua string) {
     r.ParseForm()
-    if !r.PostForm.Has("t") {
-    } else {
+    if r.PostForm.Has("t") {
         t := r.PostFormValue("t")
         var d string
         if t == "" {
@@ -73,7 +71,7 @@ func handlePost(w http.ResponseWriter, r *http.Request, f string, p string, ua s
 
 func handleGet(w http.ResponseWriter, r *http.Request, f string, p string, ua string) {
     t, _ := os.ReadFile(f)
-    if strings.HasPrefix(ua, "curl") || strings.HasPrefix(ua, "Wget") || r.URL.Query().Has("raw") {
+    if regexp.MustCompile(`^(curl|Wget).*`).MatchString(ua) || r.URL.Query().Has("raw") {
         w.Header().Set("Content-type", "text/plain; charset=UTF-8")
         w.Write(t)
     } else {
