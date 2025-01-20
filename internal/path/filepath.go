8f8793ea00c0f94a25356b1f53dfd5329c1e2760
nibazshab <44338441+nibazshab@users.noreply.github.com>
2025-01-20 15:13:49 +0800

    allow abs datapath

--- internal/path/filepath.go
+++ internal/path/filepath.go
@@ -12,14 +12,17 @@ var dataPath string
 
 func GetFilePath(filename string) string {
 	if dataPath == "" {
-		exePath, err := os.Executable()
-		if err != nil {
-			log.Fatalf("data path error: %v", err)
+		if filepath.IsAbs(*flag.Path) {
+			dataPath = filepath.Clean(*flag.Path)
+		} else {
+			exePath, err := os.Executable()
+			if err != nil {
+				log.Fatalf("data path error: %v", err)
+			}
+			dataPath = filepath.Join(filepath.Dir(exePath), *flag.Path)
 		}
 
-		dataPath = filepath.Join(filepath.Dir(exePath), *flag.Path)
-
-		if _, err = os.Stat(dataPath); os.IsNotExist(err) {
+		if _, err := os.Stat(dataPath); os.IsNotExist(err) {
 			err = os.MkdirAll(dataPath, 0o755)
 			if err != nil {
 				log.Fatalf("data path create error: %v", err)
